9d03afd4bc4b3982f6dbacf7c5ed8b40afc05c3f
Ruqul Adam Himawan 28RPL <ruqul_adam_28rpl@student.smktelkom-mlg.sch.id>
2021-11-11 21:30:18 +0700

    pp

---
+++ app/src/main/java/com/example/modul_spp_ukk2021/LoginPetugas.java
@@ -0,0 +1,14 @@
+package com.example.modul_spp_ukk2021;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPetugas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_petugas);
+    }
+}
--- app/src/main/java/com/example/modul_spp_ukk2021/MainActivity.java
+++ app/src/main/java/com/example/modul_spp_ukk2021/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.modul_spp_ukk2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button btnSiswa = findViewById(R.id.btn_siswa);
+        Button btnPetugas = findViewById(R.id.btn_petugas);
+
+        btnSiswa.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent masukSiswa = new Intent(MainActivity.this,LoginSiswa.class);
+                view.getContext().startActivity(masukSiswa);}
+        });
+
+        btnPetugas.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent masukPetugas = new Intent(MainActivity.this,LoginPetugas.class);
+                view.getContext().startActivity(masukPetugas);}
+        });
     }
 }
