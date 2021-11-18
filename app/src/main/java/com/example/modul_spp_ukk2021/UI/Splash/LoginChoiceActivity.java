ef27a699c3ba4dcbfe44b53d20b2916bd2420b25
Devon <grandthefauto77@gmail.com>
2021-11-18 10:49:27 +0700

    Combining #2

--- app/src/main/java/com/example/modul_spp_ukk2021/UI/Splash/LoginChoiceActivity.java
+++ app/src/main/java/com/example/modul_spp_ukk2021/UI/Splash/LoginChoiceActivity.java
@@ -16,16 +16,16 @@ public class LoginChoiceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_choice);
+        setContentView(R.layout.activity_kurang_bayar);
 
-        MaterialButton btnLoginSiswa = findViewById(R.id.btn_petugas);
-        btnLoginSiswa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginChoiceActivity.this, BottomNavigation.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+//        MaterialButton btnLoginSiswa = findViewById(R.id.btn_petugas);
+//        btnLoginSiswa.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(LoginChoiceActivity.this, BottomNavigation.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
     }
 }
