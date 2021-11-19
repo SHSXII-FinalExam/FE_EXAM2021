e1f3d5c0ff303f0c94a145aed38f11c577c91fee
Ruqul Adam Himawan 28RPL <ruqul_adam_28rpl@student.smktelkom-mlg.sch.id>
2021-11-19 08:11:44 +0700

    sakarepmu-devon

---
+++ app/src/main/java/com/example/modul_spp_ukk2021/DataSiswa.java
@@ -0,0 +1,13 @@
+package com.example.modul_spp_ukk2021;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DataSiswa extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data_siswa);
+    }
+}
---
+++ app/src/main/java/com/example/modul_spp_ukk2021/HomeSiswa.java
@@ -0,0 +1,13 @@
+package com.example.modul_spp_ukk2021;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeSiswa extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_siswa);
+    }
+}
