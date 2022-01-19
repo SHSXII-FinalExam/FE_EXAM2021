61aff36f9deccb42427c6a363c17e49520c0bce6
Devon <grandthefauto77@gmail.com>
2022-01-19 09:15:45 +0700

    Layout ContainerDataSiswa Update

--- app/src/main/java/com/example/modul_spp_ukk2021/UI/Network/BaseURL.java
+++ app/src/main/java/com/example/modul_spp_ukk2021/UI/Network/BaseURL.java
@@ -1,5 +1,5 @@
 package com.example.modul_spp_ukk2021.UI.Network;
 
 public class BaseURL {
-    public static final String url = "http://192.168.108.178/espepe/";
+    public static final String url = "http://192.168.43.59/espepe/";
 }
