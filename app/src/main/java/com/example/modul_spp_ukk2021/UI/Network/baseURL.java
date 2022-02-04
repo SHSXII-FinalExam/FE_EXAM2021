a7f10d77df1d44c1e1e8f69c8adda6df25734721
LethanAxeger <53499521+lethanaxeger@users.noreply.github.com>
2022-02-04 20:04:47 +0700

    Standard API Link

--- app/src/main/java/com/example/modul_spp_ukk2021/UI/Network/baseURL.java
+++ app/src/main/java/com/example/modul_spp_ukk2021/UI/Network/baseURL.java
@@ -1,5 +1,5 @@
 package com.example.modul_spp_ukk2021.UI.Network;
 
 public class baseURL {
-    public static final String url = "http://10.213.10.206//espepe/";
+    public static final String url = "http://fervent-thunder-96267.pktriot.net/spp/";
 }
