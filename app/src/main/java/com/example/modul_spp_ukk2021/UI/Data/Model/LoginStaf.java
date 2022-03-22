02f75f25fb38abbde8c72223d36a27681c06b46e
riyoda700 <66209095+riyoda700@users.noreply.github.com>
2022-03-22 12:18:36 +0700

    change

---
+++ app/src/main/java/com/example/modul_spp_ukk2021/UI/Data/Model/LoginStaf.java
@@ -0,0 +1,24 @@
+package com.example.modul_spp_ukk2021.UI.Data.Model;
+
+public class LoginStaf {
+    String username;
+    String nama_petugas;
+    String password;
+    String level;
+
+    public String getusename() {
+        return username;
+    }
+
+    public String getnama_petugas() {
+        return nama_petugas;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+}
