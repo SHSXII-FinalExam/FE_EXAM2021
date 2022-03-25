d7ab8c46f7893c0eb1039c2a30f2b434d9be912f
LethanAxeger <fadlillah_bashir_28rpl@student.smktelkom-mlg.sch.id>
2022-03-25 22:00:42 +0700

    Fixing UI Login

--- app/src/main/java/com/example/modul_spp_ukk2021/UI/Splash/LoginChoiceActivity.java
+++ app/src/main/java/com/example/modul_spp_ukk2021/UI/Splash/LoginChoiceActivity.java
@@ -19,10 +19,6 @@ public class LoginChoiceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_choice);
 
-        MaterialButton btnLoginAdmin = findViewById(R.id.btn_admin);
-        MaterialButton btnLoginPetugas = findViewById(R.id.btn_petugas);
-        MaterialButton btnLoginSiswa = findViewById(R.id.btn_siswa);
-
         btnLoginSiswa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,22 +28,18 @@ public class LoginChoiceActivity extends AppCompatActivity {
             }
         });
 
-        btnLoginPetugas.setOnClickListener(new View.OnClickListener() {
+        btnLoginStaff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginChoiceActivity.this, LoginPetugasActivity.class);
+                Intent intent = new Intent(LoginChoiceActivity.this, LoginStaffActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
+    }
 
-        btnLoginAdmin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginChoiceActivity.this, LoginAdminActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
     }
 }
