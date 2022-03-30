7ded0c5a18849db827fe0d9b9c485c972b0ca11c
riyoda700 <66209095+riyoda700@users.noreply.github.com>
2022-03-30 21:17:10 +0700

    adf

--- app/src/main/java/com/example/modul_spp_ukk2021/UI/Home/punyaPetugas/SPPRepository.java
+++ app/src/main/java/com/example/modul_spp_ukk2021/UI/Home/punyaPetugas/SPPRepository.java
@@ -1,9 +1,7 @@
 package com.example.modul_spp_ukk2021.UI.Home.punyaPetugas;
 
-import java.util.List;
-
 public class SPPRepository {
-    public List<SPP> getResult() {
-        return null;
+    public void getResult() {
+
     }
 }
