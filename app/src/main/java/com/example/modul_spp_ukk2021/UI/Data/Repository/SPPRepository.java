992e55404541abb310fe895423c8357c46bd985b
riyoda700 <66209095+riyoda700@users.noreply.github.com>
2022-03-30 21:25:14 +0700

    ad

--- app/src/main/java/com/example/modul_spp_ukk2021/UI/Data/Repository/SPPRepository.java
+++ app/src/main/java/com/example/modul_spp_ukk2021/UI/Data/Repository/SPPRepository.java
@@ -1,7 +1,23 @@
 package com.example.modul_spp_ukk2021.UI.Data.Repository;
 
-public class SPPRepository {
-    public void getResult() {
+import com.example.modul_spp_ukk2021.UI.Data.Model.SPP;
 
+import java.util.List;
+
+public class SPPRepository {
+    String value;
+    String message;
+    List<SPP> result;
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<SPP> getResult() {
+        return result;
     }
 }
