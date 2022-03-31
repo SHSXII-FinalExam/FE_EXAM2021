41f18c620a783b07095b766802373f3c35e90289
riyoda700 <66209095+riyoda700@users.noreply.github.com>
2022-03-31 10:11:46 +0700

    sdg

--- app/src/main/java/com/example/modul_spp_ukk2021/UI/UI/Home/punyaAdmin/HomeAdminActivity.java
+++ app/src/main/java/com/example/modul_spp_ukk2021/UI/UI/Home/punyaAdmin/HomeAdminActivity.java
@@ -1,7 +1,7 @@
 package com.example.modul_spp_ukk2021.UI.UI.Home.punyaAdmin;
 
 public class HomeAdminActivity {
-    public abstract enum FragmentRefreshListener {
+    public enum FragmentRefreshListener {
         ;
 
         public abstract void onRefresh();
