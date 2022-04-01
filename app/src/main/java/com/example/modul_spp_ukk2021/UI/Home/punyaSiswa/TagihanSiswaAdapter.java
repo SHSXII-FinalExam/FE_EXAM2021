65e323491353166a540de15f23fa1c82264a0bda
LethanAxeger <53499521+lethanaxeger@users.noreply.github.com>
2022-04-01 20:52:39 +0700

    Done punyaSiswa tinggal UI Kampret
    Siswa tinggal UI Modification
    Lanjutkan bagian Admin & Petugas

---
+++ app/src/main/java/com/example/modul_spp_ukk2021/UI/Home/punyaSiswa/TagihanSiswaAdapter.java
@@ -0,0 +1,72 @@
+package com.example.modul_spp_ukk2021.UI.Home.punyaSiswa;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.modul_spp_ukk2021.R;
+import com.example.modul_spp_ukk2021.UI.Model.Pembayaran;
+
+import java.text.DateFormatSymbols;
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class TagihanSiswaAdapter extends RecyclerView.Adapter<TagihanSiswaAdapter.ViewHolder> {
+    private Context context;
+    private final List<Pembayaran> pembayaran;
+
+    public TagihanSiswaAdapter(Context context, List<Pembayaran> pembayaran) {
+        this.context = context;
+        this.pembayaran = pembayaran;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.siswa_container_tagihan, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Pembayaran pembayaran = this.pembayaran.get(position);
+
+        Locale localeID = new Locale("in", "ID");
+        NumberFormat format = NumberFormat.getCurrencyInstance(localeID);
+        format.setMaximumFractionDigits(0);
+
+        DateFormatSymbols symbols = new DateFormatSymbols(localeID);
+        String[] monthNames = symbols.getMonths();
+        holder.tvBulan.setText(monthNames[pembayaran.getBulan_bayar() - 1]);
+
+        if (pembayaran.getKurang_bayar() == 0) {
+            holder.tvStatus.setText(pembayaran.getStatus_bayar());
+            holder.tvNominal.setText(format.format(pembayaran.getNominal()));
+        } else {
+            holder.tvStatus.setText("Kurang");
+            holder.tvNominal.setText(format.format(pembayaran.getKurang_bayar()));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return pembayaran.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvBulan, tvNominal, tvStatus;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tvStatus = itemView.findViewById(R.id.status);
+            tvNominal = itemView.findViewById(R.id.nominal);
+            tvBulan = itemView.findViewById(R.id.namaBulan);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/modul_spp_ukk2021/UI/Home/punyaSiswa/TagihanSiswaFragment.java
@@ -0,0 +1,110 @@
+package com.example.modul_spp_ukk2021.UI.Home.punyaSiswa;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.modul_spp_ukk2021.R;
+import com.example.modul_spp_ukk2021.UI.Network.ApiEndPoints;
+import com.example.modul_spp_ukk2021.UI.Model.Pembayaran;
+import com.example.modul_spp_ukk2021.UI.Repository.PembayaranRepository;
+
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.example.modul_spp_ukk2021.UI.Network.baseURL.url;
+
+public class TagihanSiswaFragment extends Fragment {
+    private TextView nama;
+    private RecyclerView recyclerView;
+    private TagihanSiswaAdapter adapter;
+
+    public TagihanSiswaFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.siswa_fragment_tagihan, container, false);
+
+        nama = view.findViewById(R.id.nama);
+//        kelas = view.findViewById(R.id.kelas);
+
+        recyclerView = view.findViewById(R.id.recyclerTagihan);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        ((HomeSiswaActivity) getActivity()).setTagihanRefreshListener(new HomeSiswaActivity.FragmentRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadDataTagihan();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadDataTagihan();
+    }
+
+    private void loadDataTagihan() {
+        String nisnSiswa = requireActivity().getIntent().getStringExtra("nisnSiswa");
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ApiEndPoints api = retrofit.create(ApiEndPoints.class);
+
+        Call<PembayaranRepository> call = api.viewTagihan(nisnSiswa);
+        call.enqueue(new Callback<PembayaranRepository>() {
+            @Override
+            public void onResponse(Call<PembayaranRepository> call, Response<PembayaranRepository> response) {
+                String value = response.body().getValue();
+                String message = response.body().getMessage();
+                List<Pembayaran> results = response.body().getResult();
+
+                Log.e("value", value);
+                if (value.equals("1")) {
+                    adapter = new TagihanSiswaAdapter(getActivity(), results);
+                    recyclerView.setAdapter(adapter);
+
+                    for (int i = 0; i < results.size(); i++) {
+                        nama.setText(results.get(i).getNama());
+//                        kelas.setText("Siswa " + results.get(i).getNama_kelas());
+                    }
+
+                    Locale localeID = new Locale("in", "ID");
+                    NumberFormat format = NumberFormat.getCurrencyInstance(localeID);
+                    format.setMaximumFractionDigits(0);
+
+                } else {
+                    Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PembayaranRepository> call, Throwable t) {
+                Log.e("DEBUG", "Error: ", t);
+            }
+        });
+    }
+}
