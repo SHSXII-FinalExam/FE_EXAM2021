1ddaeeeabd7505893a966d6c9643db7834862fb2
Christiany Kikyo G. M <christianykikyogracia@gmail.com>
2022-04-05 21:06:50 +0700

    Mengrapihkan Codingan

--- app/src/main/java/com/example/modul_spp_ukk2021/UI/DB/BaseURL.java
+++ app/src/main/java/com/example/modul_spp_ukk2021/UI/DB/BaseURL.java
@@ -1,5 +1,5 @@
     package com.example.modul_spp_ukk2021.UI.DB;
 
 public class BaseURL {
-    public static final String url = "https://fervent-thunder-96267.pktriot.net/spp/";
+    public static final String url = "https://nameless-sky-70925.pktriot.net/API_Modul-SPP_UKK2021/";
 }
