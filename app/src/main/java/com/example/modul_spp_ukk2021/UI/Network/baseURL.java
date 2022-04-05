d98c1feaa5003b3c319b8526cbf2a86b9eeaf75a
LethanAxeger <fadlillah_bashir_28rpl@student.smktelkom-mlg.sch.id>
2022-04-05 22:18:02 +0700

    duh error

--- app/src/main/java/com/example/modul_spp_ukk2021/UI/Network/baseURL.java
+++ app/src/main/java/com/example/modul_spp_ukk2021/UI/Network/baseURL.java
@@ -2,4 +2,6 @@ package com.example.modul_spp_ukk2021.UI.Network;
 
 public class baseURL {
     public static final String url = "https://fervent-thunder-96267.pktriot.net/spp/";
+//    public static final String url = "https://awesome-moon-22002.pktriot.net/API_Modul-SPP_UKK2021/";
+//    public static final String url = "http://localhost/API_Modul-SPP_UKK2021/";
 }
